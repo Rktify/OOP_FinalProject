1a2297d931b443c616f171fbe28f74258ccf8bc7
Wilbert <91464358+Rktify@users.noreply.github.com>
2022-06-19 22:21:19 +0700

    Update Keyboard.java

--- input/Keyboard.java
+++ input/Keyboard.java
@@ -6,32 +6,32 @@ import java.awt.event.KeyListener;
 public class Keyboard implements KeyListener{
 
 	public static boolean[] keys = new boolean[120];
+	//Holds the state of 120 keys (true if they're down, false if they're not).
+
 	public static boolean[] lastKeys = new boolean[120];
 	
-	public void update() {
+	public void update() { // To update the keys, last keys are one update after the keys
 		for(int i = 0; i < keys.length; i++) {
 			lastKeys[i] = keys[i];
 		}
 	}
-	
-	public static boolean key(int key) {
-		return keys[key];
-	}
+
+	//Helper methods
 	public static boolean keyDown(int key) {
-		return keys[key] && !lastKeys[key];
+		return keys[key] && lastKeys[key] == false;
 	}
 	public static boolean keyUp(int key) {
-		return !keys[key] && lastKeys[key];
-	}
-	
-	public void keyTyped(KeyEvent e) {
-		
+		return keys[key] == false && lastKeys[key];
 	}
 
+	@Override
+	public void keyTyped(KeyEvent e) {
+	}
+	@Override
 	public void keyPressed(KeyEvent e) {
 		keys[e.getKeyCode()] = true;
 	}
-
+	@Override
 	public void keyReleased(KeyEvent e) {
 		keys[e.getKeyCode()] = false;
 	}	
